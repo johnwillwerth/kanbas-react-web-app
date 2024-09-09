5d2ce37e1d7f7072e010de5b0ebe50c2aa7f09ee
John Willwerth <willwerth.j@northeastern.edu>
2024-09-09 15:33:13 -0400

    a1 lab img alt update

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -152,12 +152,12 @@ tag to tell browsers to render the gaps.
         <br />
         <img id="wd-starship"
           width="400px"
-        src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+        src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg" alt="Starship"
         />
         <br />
         Loading a local image:
         <br />
-        <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" />
+        <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" alt="Tesla bot" />
       </div>
       <div id="wd-forms">
         <h4>Form Elements</h4>
