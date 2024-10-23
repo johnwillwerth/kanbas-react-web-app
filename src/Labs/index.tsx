7a91f789f54fc57513afd62491de8e4486aa3ab5
John Willwerth <arw1317@protonmail.com>
2024-10-23 18:51:16 -0400

    lab4

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,28 +1,32 @@
 import { Route, Routes, Navigate } from "react-router";
+import { Provider } from "react-redux";
 import LabNavigation from "./Navigation";
 import TOC from "./TOC";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
+import store from "./store";
 import "../styles.css";
 export default function Labs() {
   return (
-    <div id="wd-labs">
-      <LabNavigation />
+    <Provider store={store}>
+      <div id="wd-labs">
+        <LabNavigation />
         <div className="wd-main-content-offset p-3">
           <h1>Labs</h1>
           <h2>John Willwerth</h2>
           <h3>CS5610 20595 - SEC 02, Fall 2024</h3>
           <TOC />
-            <Routes>
-              <Route path="/" element={<Navigate to="TOC" />} />
-              <Route path="Lab1" element={<Lab1 />} />
-              <Route path="Lab2" element={<Lab2 />} />
-              <Route path="Lab3/*" element={<Lab3 />} />
-              <Route path="Lab4/*" element={<Lab4 />} />
-            </Routes>
+          <Routes>
+            <Route path="/" element={<Navigate to="TOC" />} />
+            <Route path="Lab1" element={<Lab1 />} />
+            <Route path="Lab2" element={<Lab2 />} />
+            <Route path="Lab3/*" element={<Lab3 />} />
+            <Route path="Lab4/*" element={<Lab4 />} />
+          </Routes>
         </div>
-    </div>
+      </div>
+    </Provider>
   );
 }
