e79e65346f5fed68c64f6bddd35ffe1027cefb17
John Willwerth <arw1317@protonmail.com>
2024-10-29 15:15:26 -0400

    4

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -7,6 +7,7 @@ import Calendar from "./Calendar";
 import Inbox from "./Inbox";
 import "../styles.css";
 import * as db from "./Database";
+import ProtectedRoute from "./Account/ProtectedRoute";
 import { useState } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
@@ -43,15 +44,21 @@ export default function Kanbas() {
               <Route path="/" element={<Navigate to="Account" />} />
               <Route path="/Account/*" element={<Account />} />
               <Route path="/Dashboard" element={
-                <Dashboard 
-                  courses={courses}
-                  course={course}
-                  setCourse={setCourse}
-                  addNewCourse={addNewCourse}
-                  deleteCourse={deleteCourse}
-                  updateCourse={updateCourse}/>
+                <ProtectedRoute>
+                  <Dashboard 
+                    courses={courses}
+                    course={course}
+                    setCourse={setCourse}
+                    addNewCourse={addNewCourse}
+                    deleteCourse={deleteCourse}
+                    updateCourse={updateCourse}/>
+                </ProtectedRoute>
               } />
-              <Route path="/Courses/:cid/*" element={<Courses courses={courses} />} />
+              <Route path="/Courses/:cid/*" element={
+                <ProtectedRoute>
+                  <Courses courses={courses} />
+                </ProtectedRoute>
+              } />
               <Route path="/Calendar" element={<h1>Calendar</h1>} />
               <Route path="/Inbox" element={<h1>Inbox</h1>} />
             </Routes>
