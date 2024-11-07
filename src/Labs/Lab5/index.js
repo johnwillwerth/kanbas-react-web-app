68a91578efeac06a39e878c3682bbd9968ada754
John Willwerth <arw1317@protonmail.com>
2024-11-07 15:41:14 -0500

    a

--- src/Labs/Lab5/index.js
+++ src/Labs/Lab5/index.js
@@ -1,6 +1,13 @@
-export default function Lab5(app) {        // accept app reference to express module
-  app.get("/lab5/welcome", (req, res) => { // create route to welcome users to Lab 5.
-  res.send("Welcome to Lab 5");            // Here we are using the new arrow function syntax
-  });
-  };
-  
+export default function Lab5() {
+  return (
+  <div id="wd-lab5">
+  <h2>Lab 5</h2>
+  <div className="list-group">                 {/* hyperlink navigates to */}
+  <a href="http://localhost:4000/lab5/welcome" // http://localhost:4000/lab5/welcome
+  className="list-group-item">
+  Welcome
+  </a>
+  </div><hr/>
+  </div>
+  );
+  }
