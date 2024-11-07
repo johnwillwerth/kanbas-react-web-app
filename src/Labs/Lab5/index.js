550f0d2c0ea0ab7fbfc8ec991a7e4da07d8d332a
John Willwerth <arw1317@protonmail.com>
2024-11-07 14:45:47 -0500

    d

--- src/Labs/Lab5/index.js
+++
@@ -1,6 +0,0 @@
-export default function Lab5(app) {        // accept app reference to express module
-  app.get("/lab5/welcome", (req, res) => { // create route to welcome users to Lab 5.
-  res.send("Welcome to Lab 5");            // Here we are using the new arrow function syntax
-  });
-  };
-  
